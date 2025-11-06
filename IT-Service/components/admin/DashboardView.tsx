3a891a5934d4cc5d174f3d1891c1ba145e949318
quangtmd <39898145+quangtmd@users.noreply.github.com>
2025-11-06 21:23:26 +0700

    refactor: Clean up project files
    Removes unused code and configuration files, standardizes project structure.

--- IT-Service/components/admin/DashboardView.tsx
+++
@@ -1,3 +0,0 @@
-// This file is obsolete and has been replaced by AccountingDashboard.tsx
-// The content is removed to prevent conflicts.
-// The routing in AdminPage.tsx has been updated to use the new dashboard.
--- components/home/HomePage.tsx
+++
@@ -1,3 +0,0 @@
-// This file is an old, unused version of the homepage.
-// The primary homepage component is now located at /pages/HomePage.tsx.
-// This file can be safely deleted.
