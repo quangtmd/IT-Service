451de18947b136957443c0549239672f5e3036c0
quangtmd <39898145+quangtmd@users.noreply.github.com>
2025-12-09 19:33:31 +0700

    fix: Correct module path aliases
    Updates module path aliases in Vite and TypeScript configurations to correctly resolve imports from the 'src' directory. This ensures that all internal imports using the '@' alias are accurately mapped to the project's source files.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         plugins: [react()],
         resolve: {
             alias: {
-                '@': path.resolve(__dirname, './'),
+                '@': path.resolve(__dirname, './src'),
             },
         },
         server: {
